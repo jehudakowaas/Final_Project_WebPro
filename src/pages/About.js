16c2da416dbc0ad31bcc8710f923de91d1fded91
jehudakowaas <70264029+jehudakowaas@users.noreply.github.com>
2021-12-01 05:29:01 +0800

    init

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,10 +9,14 @@ const About = () => {
 
       <div className="container my-5">
         <h1>About this Application</h1>
-        <p>Nama Lengkap (NIM), Program Studi</p>
+        <p>This app is used to search for your favorite TV Shows/Series</p>
+        <p>There are 3 features, including: Get data, Search data, and View details</p>
+        <p> Jerushalem Kowaas(105011910048) | Fakultas Ilmu Komputer | Information System | Batch 2019 </p>
       </div>
 
-      <Footer />
+        <div>
+        <Footer />
+        </div> 
     </>
   );
 }
